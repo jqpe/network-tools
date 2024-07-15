8a8631feb9edbdf0be88a7f527717a2bc92b54ff
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-16 02:02:40 +0300

    feat: less layout shifty

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,20 @@
 import { Link } from '~/components/ui/link.tsx'
 import { ThemeToggle } from '~/components/theme-toggle'
+import { cn } from '~/utils'
 
 export function Header() {
+  const linkStyles = cn('[&.active]:underline no-underline')
+
   return (
     <header className="max-w-prose mx-auto flex justify-between py-4">
       <nav className="flex gap-2 items-center">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to="/" className={linkStyles}>
           Home
         </Link>
-        <Link to="/tld" className="[&.active]:font-bold">
+        <Link to="/tld" className={linkStyles}>
           Top level domains
         </Link>
-        <Link to="/whois" className="[&.active]:font-bold">
+        <Link to="/whois" className={linkStyles}>
           Whois
         </Link>
       </nav>
