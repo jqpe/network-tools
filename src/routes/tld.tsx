a4677921588f6618beb91ef241ad753880ea9aa4
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-15 11:53:04 +0300

    feat: popover & submit to view nearest tld

--- src/routes/tld.tsx
+++ src/routes/tld.tsx
@@ -1,21 +1,21 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
-import React from 'react'
 import { Info } from 'lucide-react'
+import React from 'react'
 
 import { TldList } from '~/components/tld-list.tsx'
+import { useNavigate } from '@tanstack/react-router'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '~/components/ui/popover'
 import { INITIAL_TLD_FILTER } from '~/constants'
 import { useIana } from '~/services/iana'
 import { IanaTld } from '~/types/iana.ts'
-import {
-  DialogTitle,
-  DialogTrigger,
-  Dialog,
-  DialogContent,
-  DialogDescription,
-} from '~/components/ui/dialog'
 
 const TldPage = () => {
+  const navigate = useNavigate({ from: '/tld' })
   const [filter, setFilter] = React.useState<RegExp>(INITIAL_TLD_FILTER)
   const ianaQuery = useIana()
 
@@ -31,6 +31,16 @@ const TldPage = () => {
     event.preventDefault()
     const { value } = event.currentTarget
     setFilter(value ? new RegExp(value, 'gi') : INITIAL_TLD_FILTER)
+  }
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault()
+
+    const tld = tlds.filter(tldFilterPredicate).at(0)
+
+    if (tld) {
+      navigate({ to: '$tld', params: { tld: tld.punycode ?? tld.unicode } })
+    }
   }
 
   if (ianaQuery.isLoading) {
@@ -49,35 +59,33 @@ const TldPage = () => {
         All top level domains in the root zone, updated <b>{updatedAt}</b>
       </p>
 
-      {/* TODO: a good feature would be to open first link on submit*/}
-      <form onSubmit={event => event.preventDefault()} className="flex gap-2">
+      <form onSubmit={onSubmit} className="flex gap-2">
         <input
+          id="tld"
+          name="tld"
           onInput={onInput}
           className="block border-slate-200 border-2 rounded-sm my-2 px-0.5 w-full"
           placeholder="search with a regular expression"
         />
-        <Dialog>
-          <DialogTrigger>
+        <Popover>
+          <PopoverTrigger>
             <Info />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Regular expression search</DialogTitle>
-            <DialogDescription className="prose">
-              <p>The regular expression is global and case-insensitive.</p>
-              <p>
-                For example, to list domains ending with AA type{' '}
-                <code>aa$</code>
-              </p>
-              <p>
-                To list domains beginning with GOO type <code>^goo</code>
-              </p>
-              <p>
-                To fuzzily match any top level domain that contains OOK type{' '}
-                <code>ook</code>
-              </p>
-            </DialogDescription>
-          </DialogContent>
-        </Dialog>
+          </PopoverTrigger>
+          <PopoverContent className="prose *:my-1">
+            <h4>Regular expression search</h4>
+            <p>The regular expression is global and case-insensitive.</p>
+            <p>
+              For example, to list domains ending with AA type <code>aa$</code>
+            </p>
+            <p>
+              To list domains beginning with GOO type <code>^goo</code>
+            </p>
+            <p>
+              To fuzzily match any top level domain that contains OOK type{' '}
+              <code>ook</code>
+            </p>
+          </PopoverContent>
+        </Popover>
       </form>
 
       <TldList tlds={tlds} filter={tldFilterPredicate} />
