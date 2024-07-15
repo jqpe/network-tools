207e47673a98f27e0c3d2d396b707467130548f8
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-15 21:52:39 +0300

    refactor: use create link utility
    Goodie that this was documented (🧢)

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,16 +1,8 @@
-import { Link as RouterLink, LinkComponent } from '@tanstack/react-router'
+import { createLink } from '@tanstack/react-router'
 import { cn } from '~/utils'
 
-export const Link: typeof RouterLink = props => {
-  // prettier-ignore
-  // @ts-expect-error If this does not error, remove the workaround (will be tree shaken!)
-  let _ = <RouterLink {...props} className={cn('underline', className)} /> as never
-
-  // <RouterLink {...props} className="" /> does not work.
-  // Save the className into a variable and delete the original reference
-  // so that base styles are not overridden
-  const { className } = props
-  delete props.className
-
-  return <RouterLink className={cn('underline', className)} {...props} />
+const LinkStyles = (props: JSX.IntrinsicElements['a']) => {
+  return <a {...props} className={cn('underline', props.className)} />
 }
+
+export const Link = createLink(LinkStyles)
