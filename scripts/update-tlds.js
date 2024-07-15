53cb84c47dc372093aea9c27016d0cb2d8954183
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-16 00:16:49 +0300

    chore: update tlds script, update tlds

---
+++ scripts/update-tlds.js
@@ -0,0 +1,11 @@
+import { writeFileSync } from 'node:fs'
+
+const res = await fetch('https://data.iana.org/rdap/dns.json')
+const json = await res.json()
+
+if (json && json.services) {
+  writeFileSync('./public/tlds.json', JSON.stringify(json.services))
+  console.log('Updated public/tlds.json')
+} else {
+  console.error("Couldn't get tlds")
+}
