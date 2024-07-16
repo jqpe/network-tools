9601ec0c4a5ff791ab08e475ab58e0f76732a666
Jasper Nykänen <jasperjnykanen@gmail.com>
2024-07-16 16:23:20 +0300

    build: move misconfigured deps to prod deps, update plugins

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { viteWranglerSpa } from '@torchauth/vite-plugin-wrangler-spa'
 
 const TESTING = process.env.NODE_ENV === 'test'
+const DEV = process.env.NODE_ENV === 'development'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,6 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     !TESTING && TanStackRouterVite(),
-    !TESTING && viteWranglerSpa(),
+    DEV && viteWranglerSpa(),
   ],
 })
